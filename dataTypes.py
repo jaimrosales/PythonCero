71f5d39f5a8724ca7df74ea4df5b3497beb62518
jaimrosales <jaimrosales@tesla.com>
2025-09-04 05:23:15 -0500

    tipos de datos

---
+++ dataTypes.py
@@ -0,0 +1,45 @@
+#numericos
+intVar = 10
+floatVar = 20.5
+complexVar = 1 + 2j
+millon = 1e6 # notacion cientifica para 1*10^6
+
+
+# print(type(intVar))
+# print(intVar)
+# print(type(floatVar)) 
+# print(floatVar)  
+# print(type(complexVar))
+# print(complexVar)
+# print(complexVar.real) #parte real
+# print(complexVar.imag) #parte imaginaria
+# print(type(millon))
+# print(str(millon)+" = 1e6") #convertir a cadena de texto
+
+# #cadenas de texto
+# stringVar = "Hola Mundo"
+# print(type(stringVar))
+# textoMultilinea = """Esto es un texto
+# multilinea, es decir que
+# puede ocupar varias lineas, sin necesidad de usar
+# caracteres especiales como \n
+# o tabuladores \t"""
+# print(textoMultilinea)
+# textoMultilinea2 = '''Esto es un texto
+# multilinea, es decir que
+# puede ocupar varias lineas, sin necesidad de usar
+# caracteres especiales como \n
+# o tabuladores \t'''
+# print(textoMultilinea2)
+name = "Jaime"
+print(name[4]) #acceder a un caracter de la cadena
+print(name[0:2]) #acceder a un rango de caracteres de la cadena
+
+#booleanos
+boolVar = True
+print(type(boolVar))
+print (bool(intVar)) #convierte a booleano, verifica si su valor es nulo
+
+
+
+
