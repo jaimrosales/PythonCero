8cb0cbbcf9c91b016e7b37b6960105bc7dc9d21d
jaimrosales <jaimrosales@tesla.com>
2025-09-04 05:31:28 -0500

    tipos de datos

---
+++ conversion.py
@@ -0,0 +1,13 @@
+#conversion de tipos de datos
+#de string a int
+num1 = "5"
+num2 = "10"
+print(type(num1))
+
+number_string = str(num1)
+print(type(number_string))
+print(type(str(num1 )))
+var = "5000"
+var2 = "jaime"
+print(int(var) + 500)
+#print(int(var2) + 500) #error
