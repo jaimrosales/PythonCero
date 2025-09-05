9571e7bdd49448666fb2234e94a7d25a8ec6ce43
jaimrosales <jaimrosales@tesla.com>
2025-09-04 21:20:37 -0500

    Sets

---
+++ Set.py
@@ -0,0 +1,7 @@
+#Sets
+# Los sets se declaran similar a los diccionarios pero sin su par
+# pero tiene la similitud con las tuplas de que no esta ordenada
+# si se agrega informacion repetida la sobreescribe, no tienes dos elementos con el mismo valor
+name = {"jaime", "ruben", "ricardo"}
+print(type(name))
+print(name)
